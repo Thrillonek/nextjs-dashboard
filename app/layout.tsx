b20a825604517dafe7bb7b11c37ff61840c40e27
Thrillonek <jindrich.kraina@gmail.com>
2026-03-15 12:37:26 +0100

    Dashboard invoices search, pagination and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import { lusitana, poppins } from '@/app/ui/fonts';
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Dashboard',
+		default: 'Dashboard',
+	},
+	description: 'The Next.js tutorial site, built with app router, whatever that is.',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
